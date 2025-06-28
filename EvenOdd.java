a35d5eac1a79cef71d0aa2fd3810a49c4b89294f
Shreya Ramnath Warkhade <shreyawarkhade2003@gmail.com>
2025-06-29 01:30:11 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,32 @@
+package ConditionsAndLoops;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        System.out.print("Enter a No.: ");
+        int num=in.nextInt();
+
+        //Brutes Method
+//        if(num%2==0){
+//            System.out.println(num+" is an even no.");
+//        }
+//        else {
+//            System.out.println(num+" is an odd no.");
+//        }
+
+
+
+//        Using Ternary Operator
+//        String status=num%2==0?"is Evan":"is Odd";
+//        System.out.println(num+ status);
+
+
+//        Using Bitwise Operator
+
+
+
+    }
+}
